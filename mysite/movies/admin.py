bc263b9c3ca9d6ff96c7b3a720e0c29e7df46f8f
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-29 21:52:03 -0800

    configure admin

--- mysite/movies/admin.py
+++ mysite/movies/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Title
 
 class TitleAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title')
+  search_fields = ['title__search', 'year']
+  list_display = ('id', 'title', 'year')
   list_filter = [ 'title', 'year' ]
 
 admin.site.register(Title, TitleAdmin)
--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Title(models.Model):
-  title = models.CharField(max_length=200)
+  title = models.TextField()
   year = models.IntegerField()
   description = models.TextField()
 
