d0fa13e28caf6b04912761f624a45e4e369cd560
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-29 20:38:15 -0800

    add models

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Title(models.Model):
+  title = models.CharField(max_length=200)
+  year = models.IntegerField()
+  description = models.TextField()
